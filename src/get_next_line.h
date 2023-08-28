6541a3740e8a9eb12436239116d54752233cd946
Sam <skalyaeve@gmail.com>
2023-08-28 06:15:29 +0200

    cleaning

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -13,13 +13,15 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFFER_SIZE 64
+
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/types.h>
 
 typedef struct	s_list
 {
-	int				fd;
+	int			fd;
 	char			*str;
 	struct s_list	*next;
 }				t_list;
@@ -29,9 +31,9 @@ int				get_in_raw(t_list ***nav, t_list **rest, int fd);
 int				until_eol(char **line, t_list **rest, t_list **begin);
 int				parsing_ended(char **line, t_list **rest, char *eol);
 int				check_rest(t_list **rest, ssize_t read_ret, t_list **begin);
-ssize_t			new_read(char **readed, t_list *rest, t_list *begin);
-void			ft_lstclear(t_list **lst);
-char			*ft_strchr(char *s, char c);
-char			*ft_strjoin(char *s1, char *s2);
+ssize_t			        new_read(char **readed, t_list *rest, t_list *begin);
+void			        ft_lstclear(t_list **lst);
+char			        *ft_strchr(char *s, char c);
+char			        *ft_strjoin(char *s1, char *s2);
 
 #endif
---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include "get_next_line.h"
+#include "stdio.h"
+
+int     main(int ac, char**av)
+{
+        char **line = malloc(sizeof(char**));
+        if (!line)
+                return (1);
+        while (get_next_line(0, line))
+        {
+                printf("%s\n", *line);
+                continue;
+        }
+        return (0);
+}
