177259078846fd08af9df234c9f302d14b17b40a
Sam <skalyaeve@gmail.com>
2023-08-30 04:59:47 +0200

    cleaning

--- include/bonus/get_next_line_bonus.h
+++ include/bonus/get_next_line_bonus.h
@@ -21,19 +21,19 @@
 
 typedef struct	s_list
 {
-	int			fd;
-	char			*str;
+	int		fd;
+	char		*str;
 	struct s_list	*next;
-}				t_list;
+}			t_list;
 
-int				get_next_line(int fd, char **line);
-int				get_in_raw(t_list ***nav, t_list **rest, int fd);
-int				until_eol(char **line, t_list **rest, t_list **begin);
-int				parsing_ended(char **line, t_list **rest, char *eol);
-int				check_rest(t_list **rest, ssize_t read_ret, t_list **begin);
-ssize_t			        new_read(char **readed, t_list *rest, t_list *begin);
-void			        ft_lstclear(t_list **lst);
-char			        *ft_strchr(char *s, char c);
-char			        *ft_strjoin(char *s1, char *s2);
+int     get_next_line(int fd, char **line);
+int     get_in_raw(t_list ***nav, t_list **rest, int fd);
+int     until_eol(char **line, t_list **rest, t_list **begin);
+int     parsing_ended(char **line, t_list **rest, char *eol);
+int     check_rest(t_list **rest, ssize_t read_ret, t_list **begin);
+ssize_t new_read(char **readed, t_list *rest, t_list *begin);
+void    ft_lstclear(t_list **lst);
+char    *ft_strchr(char *s, char c);
+char    *ft_strjoin(char *s1, char *s2);
 
 #endif
